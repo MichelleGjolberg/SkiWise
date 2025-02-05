ff7bcf17d42bbd3b4ce8595c49c6e28074fd92a7
msgjolbe <msgjolbe@stud.ntnu.no>
2025-02-04 21:19:19 -0700

    added simple interface + ignore

---
+++ frontend/app/components/UserInput.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SubmitButton from "~/components/SubmitButton";
+
+
+const UserInput: React.FC = () => {
+  return (
+    <>
+      <SubmitButton text={"Find you mountain"} /></>
+  );
+};
+
+export default UserInput;
--- frontend/app/routes/_index.tsx
+++ frontend/app/routes/_index.tsx
@@ -1,25 +1,25 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction } from '@remix-run/node';
+import UserInput from '~/components/UserInput';
+import NavBar from '~/components/NavBar';
 
 //This is the main page (all .tsx files under routes are singular pages)
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: 'New Remix App' },
+    { name: 'description', content: 'Welcome to Remix!' },
   ];
 };
 
 export default function Index() {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <div className="flex flex-col items-center gap-16">
-        <header className="flex flex-col items-center gap-9">
-          <h1 className="leading text-2xl font-bold text-gray-800 dark:text-gray-100">
-            Welcome to Skiwise
-          </h1>
-        </header>
-    
+    <div className="flex flex-col h-screen">
+      <NavBar />
+      <div className="flex flex-grow items-center justify-center flex-col">
+        <h1 className="text-2xl font-bold text-gray-800 dark:text-gray-800">
+          Welcome to Skiwise
+        </h1>
+        <UserInput />
       </div>
     </div>
   );
 }
-
