903f68ae9cb9277553b7a498cbbe19e33303918d
sierrareschke <sire7023@colorado.edu>
2025-02-13 11:57:59 -0700

    created recent snowfall backend file

---
+++ backend/recent_snowfall_backend.py
@@ -0,0 +1,8 @@
+## file to pull from a weather API 
+# will have a list of the resorts that are within the user-defined distance range and make API calls to get the recent snowfall at that location
+
+import sys
+import os
+import base64
+import jsonpickle, json
+import requests
