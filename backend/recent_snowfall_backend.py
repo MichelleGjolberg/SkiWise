ff157a3f455e9d02cba7c02f0bab716ad70b8327
sierrareschke <sire7023@colorado.edu>
2025-02-13 12:17:19 -0700

    synoptic data api token and key

--- backend/recent_snowfall_backend.py
+++ backend/recent_snowfall_backend.py
@@ -6,3 +6,13 @@ import os
 import base64
 import jsonpickle, json
 import requests
+
+# Synoptic data API
+# token: fda25733baec41ee90dd23653e94a1a8
+# key: KuOMm49ZKk4W5zFM2YEVmGID1bIaXFgJKUmJI2tekJ
+
+# snow_depth, snow_accum, precip_storm, precip_accum_six_hour, precip_accum_six_hour, snow_accum_since_7_local, snow_accum_24_hour
+
+
+API_ROOT = "https://api.synopticdata.com/v2/"
+API_TOKEN = "fda25733baec41ee90dd23653e94a1a8"
