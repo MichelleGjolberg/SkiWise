829639ed71fb1cf1937b2f8640516ff395d4fba7
msgjolbe <msgjolbe@stud.ntnu.no>
2025-04-02 22:08:44 -0600

    put prediction inside of userinput

--- frontend/app/components/UserInput.tsx
+++ frontend/app/components/UserInput.tsx
@@ -1,4 +1,8 @@
 import React, { useState } from 'react';
+import Prediction from '~/components/Prediction';
+
+//Need to fetch data for default list when page load
+//When submit boutton is clicked => new predicted data should show
 
 const UserInput: React.FC = () => {
   const [userName, setUserName] = useState('');
@@ -63,166 +67,169 @@ const UserInput: React.FC = () => {
   };
 
   return (
-    <form
-      onSubmit={handleSubmit}
-      className="flex flex-col space-y-4 p-6 mx-4 bg-white shadow-md rounded-lg max-w-[400px] max-h-[500px] overflow-y-auto"
-    >
-      {error && <p className="text-red-500 font-semibold">{error}</p>}
-      <label className="flex flex-col">
-        <span className="font-semibold">Your Name:</span>
-        <input
-          type="text"
-          placeholder="Enter your name"
-          value={userName}
-          onChange={(e) => setUserName(e.target.value)}
-          className="border rounded p-2"
-          required
-        />
-      </label>
-
-      <label className="flex flex-col">
-        <span className="font-semibold">Driving Time (minutes):</span> 
-        <input
-          type="number"
-          placeholder="Enter driving time"
-          value={distance}
-          onChange={(e) => setDistance(e.target.value)}
-          className="border rounded p-2"
-          required
-        />
-      </label>
-
-      {/* Number of People */}
-      <label className="flex flex-col">
-        <span className="font-semibold">People on the trip:</span>
-        <input
-          type="number"
-          placeholder="Enter number of people"
-          value={people}
-          onChange={(e) => setPeople(e.target.value)}
-          className="border rounded p-2"
-          required
-        />
-      </label>
-
-      {/* Max Budget */}
-      <label className="flex flex-col">
-        <span className="font-semibold">Max Budget ($):</span>
-        <input
-          type="number"
-          placeholder="Enter your budget"
-          value={budget}
-          onChange={(e) => setBudget(e.target.value)}
-          className="border rounded p-2"
-          required
-        />
-      </label>
-
-      {/* Driving Experience */}
-      <label className="flex flex-col">
-        <span className="font-semibold">Driving Experience:</span>
-        <select
-          value={drivingExperience}
-          onChange={(e) => setDrivingExperience(e.target.value)}
-          className="border rounded p-2"
-        >
-          <option value="beginner">Beginner</option>
-          <option value="intermediate">Intermediate</option>
-          <option value="expert">Expert</option>
-        </select>
-      </label>
-
-      {/* Minimum Fresh Powder */}
-      <label className="flex flex-col">
-        <span className="font-semibold">
-          Minimum fresh powder in 24 hours (inches):
-        </span>
-        <input
-          type="number"
-          placeholder="Enter inches of fresh snow"
-          value={freshPowder}
-          onChange={(e) => setFreshPowder(e.target.value)}
-          className="border rounded p-2"
-        />
-      </label>
-      {/* Type of pass */}
-      <fieldset className="flex flex-col">
-        <legend className="font-semibold">
-          Do you have an Ikon/Epic pass?
-        </legend>
-        <label className="flex items-center space-x-2">
-          <input
-            type="radio"
-            value="ikon"
-            checked={passType === 'ikon'}
-            onChange={() => setPassType('ikon')}
-          />
-          <span>Ikon Pass</span>
-        </label>
-        <label className="flex items-center space-x-2">
-          <input
-            type="radio"
-            value="epic"
-            checked={passType === 'epic'}
-            onChange={() => setPassType('epic')}
-          />
-          <span>Epic Pass</span>
-        </label>
-        <label className="flex items-center space-x-2">
-          <input
-            type="radio"
-            value="none"
-            checked={passType === 'none' && !isPayOptionSelected}
-            onChange={() => handleNoneChange('both')}
-          />
-          <span>Both Passes</span>
-        </label>
-        <label className="flex items-center space-x-2">
-          <input
-            type="radio"
-            value="none"
-            checked={passType === 'none' && isPayOptionSelected}
-            onChange={() => handleNoneChange('pay')}
-          />
-          <span>Willing to pay for a pass</span>
-        </label>
-      </fieldset>
-
-      <label className="flex flex-col">
-        <span className="font-semibold">How important is cost? (1-10)</span>
-        <input
-          type="range"
-          min="1"
-          max="10"
-          value={costImportance}
-          onChange={(e) => setCostImportance(Number(e.target.value))}
-          className="cursor-pointer"
-        />
-        <span className="text-center">{costImportance}</span>
-      </label>
-
-      <label className="flex flex-col">
-        <span className="font-semibold">
-          How important is driving time? (1-10)
-        </span>
-        <input
-          type="range"
-          min="1"
-          max="10"
-          value={timeImportance}
-          onChange={(e) => setTimeImportance(Number(e.target.value))}
-          className="cursor-pointer"
-        />
-        <span className="text-center">{timeImportance}</span>
-      </label>
-
-      <button
-        type="submit"
-        className="bg-blue-500 text-white py-2 rounded hover:bg-blue-600"
+    <div className="flex flex-grow justify-center flex-row w-full h-full bg-lightblue  py-5 mt-[-400px] ">
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col space-y-4 p-6 mx-4 bg-white shadow-md rounded-lg max-w-[400px] max-h-[500px] overflow-y-auto"
       >
-        Find your mountain
-      </button>
-    </form>
+        {error && <p className="text-red-500 font-semibold">{error}</p>}
+        <label className="flex flex-col">
+          <span className="font-semibold">Your Name:</span>
+          <input
+            type="text"
+            placeholder="Enter your name"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+            className="border rounded p-2"
+            required
+          />
+        </label>
+
+        <label className="flex flex-col">
+          <span className="font-semibold">Driving Time (minutes):</span>
+          <input
+            type="number"
+            placeholder="Enter driving time"
+            value={distance}
+            onChange={(e) => setDistance(e.target.value)}
+            className="border rounded p-2"
+            required
+          />
+        </label>
+
+        {/* Number of People */}
+        <label className="flex flex-col">
+          <span className="font-semibold">People on the trip:</span>
+          <input
+            type="number"
+            placeholder="Enter number of people"
+            value={people}
+            onChange={(e) => setPeople(e.target.value)}
+            className="border rounded p-2"
+            required
+          />
+        </label>
+
+        {/* Max Budget */}
+        <label className="flex flex-col">
+          <span className="font-semibold">Max Budget ($):</span>
+          <input
+            type="number"
+            placeholder="Enter your budget"
+            value={budget}
+            onChange={(e) => setBudget(e.target.value)}
+            className="border rounded p-2"
+            required
+          />
+        </label>
+
+        {/* Driving Experience */}
+        <label className="flex flex-col">
+          <span className="font-semibold">Driving Experience:</span>
+          <select
+            value={drivingExperience}
+            onChange={(e) => setDrivingExperience(e.target.value)}
+            className="border rounded p-2"
+          >
+            <option value="beginner">Beginner</option>
+            <option value="intermediate">Intermediate</option>
+            <option value="expert">Expert</option>
+          </select>
+        </label>
+
+        {/* Minimum Fresh Powder */}
+        <label className="flex flex-col">
+          <span className="font-semibold">
+            Minimum fresh powder in 24 hours (inches):
+          </span>
+          <input
+            type="number"
+            placeholder="Enter inches of fresh snow"
+            value={freshPowder}
+            onChange={(e) => setFreshPowder(e.target.value)}
+            className="border rounded p-2"
+          />
+        </label>
+        {/* Type of pass */}
+        <fieldset className="flex flex-col">
+          <legend className="font-semibold">
+            Do you have an Ikon/Epic pass?
+          </legend>
+          <label className="flex items-center space-x-2">
+            <input
+              type="radio"
+              value="ikon"
+              checked={passType === 'ikon'}
+              onChange={() => setPassType('ikon')}
+            />
+            <span>Ikon Pass</span>
+          </label>
+          <label className="flex items-center space-x-2">
+            <input
+              type="radio"
+              value="epic"
+              checked={passType === 'epic'}
+              onChange={() => setPassType('epic')}
+            />
+            <span>Epic Pass</span>
+          </label>
+          <label className="flex items-center space-x-2">
+            <input
+              type="radio"
+              value="none"
+              checked={passType === 'none' && !isPayOptionSelected}
+              onChange={() => handleNoneChange('both')}
+            />
+            <span>Both Passes</span>
+          </label>
+          <label className="flex items-center space-x-2">
+            <input
+              type="radio"
+              value="none"
+              checked={passType === 'none' && isPayOptionSelected}
+              onChange={() => handleNoneChange('pay')}
+            />
+            <span>Willing to pay for a pass</span>
+          </label>
+        </fieldset>
+
+        <label className="flex flex-col">
+          <span className="font-semibold">How important is cost? (1-10)</span>
+          <input
+            type="range"
+            min="1"
+            max="10"
+            value={costImportance}
+            onChange={(e) => setCostImportance(Number(e.target.value))}
+            className="cursor-pointer"
+          />
+          <span className="text-center">{costImportance}</span>
+        </label>
+
+        <label className="flex flex-col">
+          <span className="font-semibold">
+            How important is driving time? (1-10)
+          </span>
+          <input
+            type="range"
+            min="1"
+            max="10"
+            value={timeImportance}
+            onChange={(e) => setTimeImportance(Number(e.target.value))}
+            className="cursor-pointer"
+          />
+          <span className="text-center">{timeImportance}</span>
+        </label>
+
+        <button
+          type="submit"
+          className="bg-blue-500 text-white py-2 rounded hover:bg-blue-600"
+        >
+          Find your mountain
+        </button>
+      </form>
+      <Prediction />
+    </div>
   );
 };
 
