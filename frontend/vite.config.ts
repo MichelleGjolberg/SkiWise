2b58d023e46cafed4f58e6d0ad077cccb717e271
nathanvarghese <nathanvarghese948@gmail.com>
2025-04-03 08:50:21 -0600

    setup of test environment part2.0

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,11 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
+import { installGlobals } from "@remix-run/node";
+
+installGlobals();
+
+const MODE = process.env.NODE_ENV;
 
 declare module "@remix-run/node" {
   interface Future {
@@ -12,9 +17,21 @@ declare module "@remix-run/node" {
 export default defineConfig({
 
   test: {
-    environment: 'jsdom'
+    include: ["**/__tests__/**.{js,jsx,ts,tsx}"],
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './setupTests.ts',
+    restoreMocks: true,
+    // coverage: {
+    //   exclude: ["**/__tests__/**"],
+    //   include: ["app/**/*.{ts,tsx}"],
+    //   all: true,
+    // },
+    alias: {
+      "~": "/app",
+    },
   },
-  
+
   plugins: [
     remix({
       future: {
