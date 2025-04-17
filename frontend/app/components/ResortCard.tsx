ae958f98c237152c01c445c6172cce76e0d6c3ac
msgjolbe <msgjolbe@stud.ntnu.no>
2025-04-17 12:09:03 -0600

    updates

--- frontend/app/components/ResortCard.tsx
+++ frontend/app/components/ResortCard.tsx
@@ -17,7 +17,7 @@ const ResortCard: React.FC<ResortCardProps> = ({
 }) => {
   return (
     <>
-      <div className="w-100 h-32 flex flex-row items-center mx-4 mb-4 border-2 bg-slate-100 border-slate-300 rounded-lg">
+      <div className="w-100 h-32 flex flex-row items-center mx-4 mb-4 border-2 bg-white border-slate-300 rounded-lg">
         <img src={icon} alt={iconAlt} className="h-20 px-1 pl-2" />
         <div className="flex flex-col justify-center pl-4">
           <p className="font-bold"> {place}</p>
